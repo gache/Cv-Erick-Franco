f7b4c30ab170d2e0e7543b00c4072ba2348c0f46
Gache <erickfrancodelgado@hotmail.com>
2019-07-05 21:42:38 +0200

    Expériences professionnelles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
+// Components
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ProfilComponent } from './components/profil/profil.component';
+import { ParcoursComponent } from './components/parcours/parcours.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     FooterComponent,
-    ProfilComponent
+    ProfilComponent,
+    ParcoursComponent,
+
   ],
   imports: [
     BrowserModule,
