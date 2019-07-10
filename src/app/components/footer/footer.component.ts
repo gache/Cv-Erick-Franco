9013fdbaa55984c0b68fe233def64a6e40049a07
Gache <erickfrancodelgado@hotmail.com>
2019-07-10 22:14:16 +0200

    Mise en forme de la page

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -9,6 +9,7 @@ export class FooterComponent implements OnInit {
 
   annee: number;
   constructor() {
+    // Function avoir l'année de manière automatique
     this.annee = new Date().getFullYear();
   }
 
