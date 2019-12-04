93037f276e53ef3b42d296b4075263f5b8ba0cc9
alexis.bott <alexis.bott@production.infra>
2019-12-04 15:24:54 +0100

    rectifs vus avec Erick

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -9,38 +9,37 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class ContactComponent implements OnInit {
 
   formulaire: FormGroup;
-  messageAlert1:string = 'Le nom est necessaire';
-  messageAlert2:string = 'Le Prenom est necessaire';
-  messageAlert3:string = 'Le Mail est necessaire';
-  messageAlert4:string = 'Le sujet est necessaire';
-  messageAlert5:string = 'Le message est necessaire';
+  messageAlert1: string = 'Le nom est necessaire';
+  messageAlert2: string = 'Le Prenom est necessaire';
+  messageAlert3: string = 'Le Mail est necessaire';
+  messageAlert4: string = 'Le sujet est necessaire';
+  messageAlert5: string = 'Le message est necessaire';
 
   constructor() {
     this.formulaire = new FormGroup({
 
-      'nom'    : new FormControl('', [Validators.required,
-                                      Validators.minLength(3)]),
-      'prenom' : new FormControl('', [Validators.required,
-                                      Validators.minLength(3)]),
-      'mail'   : new FormControl('', [Validators.required,
-                                      Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"),
-                                      ]),
-      'sujet'  : new FormControl('', [Validators.required,
-                                      Validators.minLength(3)]),
-      'message': new FormControl('', [Validators.required,
-                                      Validators.minLength(5), Validators.maxLength(500)]),
-
+      nom: new FormControl('', [Validators.required,
+      Validators.minLength(3)]),
+      prenom: new FormControl('', [Validators.required,
+      Validators.minLength(3)]),
+      mail: new FormControl('', [Validators.required,
+      Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$'),
+      ]),
+      sujet: new FormControl('', [Validators.required,
+      Validators.minLength(3)]),
+      message: new FormControl('', [Validators.required,
+      Validators.minLength(5), Validators.maxLength(500)]),
     });
 
     this.formulaire.controls['nom'].valueChanges
-        .subscribe(data => {
-          console.log(data);
-        }
-      );
+      .subscribe(data => {
+        console.log(data);
+      }
+    );
   }
   changement() {
-   console.log(this.formulaire.value);
-   console.log(this.formulaire);
+    console.log(this.formulaire.value);
+    console.log(this.formulaire);
 
   }
 
